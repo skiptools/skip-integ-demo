4d81db6f01a995b96956aee6c46699e2aed5d010
Marc Prud'hommeaux <marc@skip.tools>
2024-12-09 16:43:29 -0500

    Set up testing

--- Tests/SkipIntegDemoTests/SkipIntegDemoTests.swift
+++ Tests/SkipIntegDemoTests/SkipIntegDemoTests.swift
@@ -1,14 +1,20 @@
 import XCTest
 import OSLog
 import Foundation
+import SkipBridgeKt
 @testable import SkipIntegDemo
 
 let logger: Logger = Logger(subsystem: "SkipIntegDemo", category: "Tests")
 
 @available(macOS 13, *)
 final class SkipIntegDemoTests: XCTestCase {
+    override func setUp() {
+        #if os(Android)
+        loadPeerLibrary(packageName: "skip-integ-demo", moduleName: "SkipIntegDemo")
+        #endif
+    }
+
     func testSkipIntegDemo() throws {
         XCTAssertEqual(SkipIntegDemoModule().stringFunction(), "Hello, World!")
     }
 }
-
